d07cd7f4b1ef00bce3ca26d90ad360ceb8dbfb47
Sajit Sasi <sajit.sasi@gmail.com>
2023-03-03 11:36:54 -0600

    Updated sed for MacOS

--- azure/aks/setDataSourceRG.sh
+++ azure/aks/setDataSourceRG.sh
@@ -3,4 +3,9 @@ set -e
 echo "setDataSourceRG.sh: getting resource group name"
 var1=$(terraform output resource_group_name)
 echo "setDataSourceRG.sh: parsing resource group name"
-sed -i "s/resource_group_name=.*/resource_group_name=$var1/g" ../eck/data-sources.tf
+kernel=$(uname -s)
+if [ "${kernel}" = "Darwin" ]; then
+  sed -i "" "s/resource_group_name=.*/resource_group_name=$var1/g" ../create-eck/data-sources.tf
+else
+  sed -i "s/resource_group_name=.*/resource_group_name=$var1/g" ../create-eck/data-sources.tf
+fi
